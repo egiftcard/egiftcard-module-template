66d1d87fc0a190f3986dab985cf550b50b73729d
FortiShield <161459699+FortiShield@users.noreply.github.com>
2024-04-19 08:55:37 +0000

    init commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,50 @@
 module.exports = {
-  env: {
-    'browser': true,
-  },
+  root: true,
 
-  extends: [
-    '@egiftcard/eslint-config',
-    '@egiftcard/eslint-config/config/typescript',
+  extends: ['@egiftcard/eslint-config'],
+
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: ['@egiftcard/eslint-config-typescript'],
+    },
+
+    {
+      files: ['*.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      extends: ['@egiftcard/eslint-config-nodejs'],
+    },
+
+    {
+      files: ['yarn.config.cjs'],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2020,
+      },
+      settings: {
+        jsdoc: {
+          mode: 'typescript',
+        },
+      },
+      extends: ['@egiftcard/eslint-config-nodejs'],
+    },
+
+    {
+      files: ['*.test.ts', '*.test.js'],
+      extends: [
+        '@egiftcard/eslint-config-jest',
+        '@egiftcard/eslint-config-nodejs',
+      ],
+    },
   ],
 
-  overrides: [{
-    files: [
-      '.eslintrc.js',
-    ],
-    parserOptions: {
-      sourceType: 'script',
-    },
-    extends: [
-      '@egiftcard/eslint-config/config/nodejs',
-    ],
-  }],
+  ignorePatterns: [
+    '!.eslintrc.js',
+    '!.prettierrc.js',
+    'dist/',
+    'docs/',
+    '.yarn/',
+  ],
 };
